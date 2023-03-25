f4b994373689e95a37831077a32590c86339ffef
Harshal Janjani <harshaljanjani@gmail.com>
2023-03-25 10:50:48 +0530

    feat: added scraping to the infinite scrolling bot

--- Python For Web Scraping And Network Programming/Web Scraping/Making Twitter Bots/Twitter Infinite Scrolling Bot (Day 13).py
+++ Python For Web Scraping And Network Programming/Web Scraping/Making Twitter Bots/Twitter Infinite Scrolling Bot (Day 13).py
@@ -17,19 +17,54 @@ driver = webdriver.Chrome(path,options=options,service=Service(ChromeDriverManag
 driver.get(website)
 driver.maximize_window()
 
-# Get the initial scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # Wait to load page
-    time.sleep(5) #TODO: Replace for explicit waits
-    # Calculate new scroll height and compare it with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:  # if the new and last height are equal, it means that there isn't any new page to load, so we stop scrolling
-        break
-    else:
-        last_height = new_height
+def get_tweet(element):
+    try:
+        user = element.find_element_by_xpath(".//span[contains(text(), '@')]").text
+        text = element.find_element_by_xpath(".//div[@lang]").text
+        tweet_data = [user, text]
+    except:
+        tweet_data = ['user', 'text'] # Dummy text
+    return tweet_data
+
+user_data = []
+text_data = []
+tweet_ids = set() # Why sets? -> avoid duplicate elements due to Twitter's lazy loading approach (selenium might end up locating duplicate elements with the same XPath)
+
+scrolling = True
+while scrolling:
+    tweets = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, "//article[@role='article']")))
+    for tweet in tweets[-15:]: # Retrieve only the last 15 tweets
+        # "tweet_id" helps filter duplicate tweets
+        tweet_list = get_tweet(tweet)
+        tweet_id = "".join(tweet_list) # Concatenate username and the tweet text
+        if tweet_id not in tweet_ids:
+            tweet_ids.add(tweet_id)
+            user_data.append(tweet_list[0])
+            text_data.append(" ".join(tweet_list[1].split()))
+            print(user_data)
+            print(text_data)
+
+    # Get the initial scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load page
+        time.sleep(5) #TODO: Replace for explicit waits
+        # Calculate new scroll height and compare it with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:  # If the new and last height are equal, it means that there isn't any new page to load, so we stop scrolling
+            scrolling = False # Break outer loop as well
+            break
+        else:
+            last_height = new_height
+            break # If this "break" wasn't present, the script would just scroll to the end of the website without scraping the data from each page. Now it scrapes on every iteration
+
 driver.quit()
+# Adding collected data to a .csv file
+df_tweets = pd.DataFrame({'user': user_data, 'text': text_data})
+df_tweets.to_csv('tweets_infinite_scrolling.csv', index=False)
+print(df_tweets)
+
 
 
