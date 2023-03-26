ae795430ba3137ba3acaaf380656c9a1274a1b93
Harshal Janjani <harshaljanjani@gmail.com>
2023-03-26 20:15:37 +0530

    feat: created MongoDB pipeline for Scrapy spider

--- Python For Web Scraping And Network Programming/Web Scraping/Worldometer Spider Using Scrapy (Day 13)/pipelines.py
+++ Python For Web Scraping And Network Programming/Web Scraping/Worldometer Spider Using Scrapy (Day 13)/pipelines.py
@@ -1,13 +1,27 @@
 # Define your item pipelines here
-#
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
-# useful for handling different item types with a single interface
+# Useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import logging
+import pymongo
 
+# MongoDB Atlas Pipeline For Storing Scraped Data Into Collections
+class MongoDBPipeline:
+    collection_name = 'transcripts'
+    # Called when spider starts running
+    def open_spider(self,spider):
+        logging.warning('Spider Opened - Pipeline')
+        self.client = pymongo.MongoClient("mongodb+srv://adminUser:adminPass@mongo-cluster.wav4pod.mongodb.net/?retryWrites=true&w=majority") # Set connection string -> This is an exemplar connection string, the original connection string has not been kept open-source.
+        self.db = self.client['My_Database']
 
-class SpiderTutorialPipeline:
+    # Destructor function (Called when spider finishes scraping process)
+    def close_spider(self,spider):
+        logging.warning('Spider Closed - Pipeline')
+        self.client.close() # Close connection with MongoDB
+
+    # Function called for each item scraped from the website
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item) # Name of collection to insert each item into
         return item
--- Python For Web Scraping And Network Programming/Web Scraping/Worldometer Spider Using Scrapy (Day 13)/settings.py
+++ Python For Web Scraping And Network Programming/Web Scraping/Worldometer Spider Using Scrapy (Day 13)/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "spider_tutorial.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "spider_tutorial (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "spider_tutorial.pipelines.SpiderTutorialPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "spider_tutorial.pipelines.MongoDBPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
