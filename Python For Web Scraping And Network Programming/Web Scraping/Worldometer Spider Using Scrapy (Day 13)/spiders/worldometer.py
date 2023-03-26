1c173841ce16b57e964df5e462a50d4c0aba2c02
Harshal Janjani <harshaljanjani@gmail.com>
2023-03-26 12:04:37 +0530

    commit#33->population data (JSON) and neat-freak

--- Python For Web Scraping And Network Programming/Web Scraping/Worldometer Spider Using Scrapy (Day 13)/spiders/worldometer.py
+++ Python For Web Scraping And Network Programming/Web Scraping/Worldometer Spider Using Scrapy (Day 13)/spiders/worldometer.py
@@ -6,11 +6,12 @@ import scrapy
 class WorldometerSpider(scrapy.Spider):
     name = "worldometer"
     allowed_domains = ["www.worldometers.info"]
-    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+    start_urls = ["https://www.worldometers.info/world-population/population-by-country/"]
 
     def parse(self, response):
         title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/').getall()
+        # Extracting "a" elements for each country
+        countries = response.xpath('//td/a')
 
         for country in countries:
             country_name = country.xpath(".//text()").get()
@@ -18,10 +19,18 @@ class WorldometerSpider(scrapy.Spider):
             # Return data extracted at each iteration
             # If we were to yield "scrapy.Request(url=link)", we'd not be able to visit the scraped link as it is, since it's in relative form.
             # Obselete way of concatenation: absolute_url = f'https://www.worldometers.info/{link}'
-            absolute_url = response.urljoin(link) 
-            yield {
-                'country_names': country_name,
-                'links': absolute_url, # Convert relative links to absolute links
+            # absolute_url = response.urljoin(link) 
+            yield response.follow(url=link, callback=self.parse_country, meta={'country':country_name}) # callback function is called everytime a new link is visited (data is scraped using the function)
+    def parse_country(self,response):
+        country = response.request.meta['country']
+        rows = response.xpath("(//table[contains(@class,'table')])[1]/tbody/tr") # list
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield{
+                "country":country,
+                "year": year,
+                "population": population
             }
             # Alternative Syntax:
             # scrapy.Request(url=absolute_url) -> 200 OK response
